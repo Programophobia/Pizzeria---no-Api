58432be2c52097b90b42fb8f27fd4063c6d671f8
Anna Powaska <annapowaska@op.pl>
2023-12-18 07:15:04 +0100

    Add price update counter in cart

--- src/js/script.js
+++ src/js/script.js
@@ -254,6 +254,7 @@
   prepareCartProduct(){
   
     const thisProduct = this;
+    
     const productSummary = {
       id: thisProduct.id,
         name: thisProduct.data.name,
@@ -405,9 +406,47 @@ class Cart{
       const generatedHTML = templates.cartProduct(menuProduct);
       const generatedDOM = utils.createDOMFromHTML(generatedHTML);
       thisCart.dom.productList.appendChild(generatedDOM);
+      thisCart.products.push(new CartProduct(menuProduct, generatedDOM));
     }
   }
   
+  class CartProduct {
+    constructor(menuProduct, element){
+      const thisCartProduct = this;
+      element = menuProduct.element;
+
+      thisCartProduct.id = menuProduct.id;
+      thisCartProduct.name = menuProduct.name; 
+      thisCartProduct.amount = menuProduct.amount;
+      thisCartProduct.priceSingle = menuProduct.priceSingle;
+      thisCartProduct.price = menuProduct.price;
+      thisCartProduct.params = menuProduct.params;   
+      
+      thisCartProduct.getElements(element);
+      thisCartProduct.initAmountWidget();
+     console.log('cart product:', thisCartProduct);
+    }
+
+    getElements(element){
+      const thisCartProduct = this;
+      thisCartProduct.dom = {};
+      thisCartProduct.dom.wrapper = element;
+      thisCartProduct.dom.amountWidget = document.querySelector(select.cartProduct.amountWidget);
+      thisCartProduct.dom.price = document.querySelector(select.cartProduct.price);
+      thisCartProduct.dom.edit = document.querySelector(select.cartProduct.edit);
+      thisCartProduct.dom.remove = document.querySelector(select.cartProduct.remove);
+    }
+
+    initAmountWidget(){
+      const thisCartProduct = this;
+      thisCartProduct.amountWidget = new AmountWidget(thisCartProduct.dom.amountWidget);
+      thisCartProduct.dom.amountWidget.addEventListener('updated', function() {
+        thisCartProduct.price = thisCartProduct.amountWidget.value * thisCartProduct.priceSingle;
+        thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
+        });
+    }
+  }
+
   const app = {
       initData: function(){
         const thisApp = this;
