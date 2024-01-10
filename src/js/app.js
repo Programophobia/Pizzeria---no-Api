e013b2afc22965f9b867c7a7fc73409ca393a5cb
Anna Powaska <annapowaska@op.pl>
2024-01-10 14:51:27 +0100

    Add proper display methods to subpages links on the side

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,74 @@
-import {settings, select} from './settings.js';
+import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
     const app = {
+    
+      initPages: function() {
+        const thisApp = this;
 
+        thisApp.pages = document.querySelector(select.containerOf.pages).children;
+        thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+        const idFromHash = window.location.hash.replace('#/', '');
+
+        let pageMatchingHash = thisApp.pages[0].id;
+
+        for(let page of thisApp.pages){
+          if(page.id == idFromHash){
+            pageMatchingHash = page.id;
+            break;
+          }
+        }
+
+        thisApp.activatePage(pageMatchingHash);
+       
+        for(let link of thisApp.navLinks){
+          link.addEventListener('click', function(event){
+            const clickedElement = this;
+            event.preventDefault();
+
+            //get page id from href
+            const id = clickedElement.getAttribute('href').replace('#', '')
+
+            // run thisApp.activePage with that id
+            thisApp.activatePage(id);
+
+            //change URL hash
+            window.location.hash = '#/' + id;
+          })
+        }
+      },
+
+      activatePage: function(pageId){
+        const thisApp = this;
+        
+        //add class active to matching pages and remove from non matching
+        for(let page of thisApp.pages){
+          page.classList.toggle(classNames.pages.active, page.id == pageId);
+        }
+
+        //add class active to matching lings, and remove from not matching
+        for(let link of thisApp.navLinks){
+          link.classList.toggle(
+            classNames.nav.active, 
+            link.getAttribute('href') == '#' + pageId
+            );
+          }
+        },
+      
       initData: function(){
         const thisApp = this;
         thisApp.data = {};
         const url = settings.db.url + '/' + settings.db.products;
-        fetch(url) 
-         .then(function(rawResponse){
+          fetch(url) 
+          .then(function(rawResponse){
           return rawResponse.json();
         })
         .then(function(parsedResponse){
-         console.log('parsedResponse', parsedResponse);
-
-        thisApp.data.products = parsedResponse
-
-        thisApp.initMenu();
+          thisApp.data.products = parsedResponse
+          thisApp.initMenu();
         });
-        console.log('thisApp.data', JSON.stringify(thisApp.data));
       },
 
       initMenu(){
@@ -44,6 +93,7 @@ import Cart from './components/Cart.js';
         const thisApp = this;
         thisApp.initData();
         thisApp.initCart();
+        thisApp.initPages();
       }
     };
   app.init();
